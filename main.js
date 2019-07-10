87282b01a7fd673f08cd3e44e7e382045de89cab
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-11 01:38:30 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,20 @@
+// +++++++++++++++logical_statement in js++++++++++++++++++++//
+
+var a = 9
+var b = 8
+var c = 5
+var d = 7
+
+if (a>b && c>d){
+    console.log(true)
+}else{
+    console.log(false)
+}
+//answer:false
+
+if(a>b || c>d){
+    console.log(false)
+}else{
+    console.log(true)
+}
+//answer:true
